cf199c3d3faaefb25849169b0381452653981aa8
nikolaswurmel <niwu8050@colorado.edu>
2018-03-11 16:55:42 -0600

    3/11 sql user database import file

---
+++ sql_import.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE db;
+USE db;
+
+CREATE TABLE users 
+(
+    'id' INT NOT NULL AUTO_INCREMENT,
+    'user' VARCHAR(50) NOT NULL,
+    'pass' VARCHAR(100) NOT NULL,
+PRIMARY KEY (`id`) 
+);
